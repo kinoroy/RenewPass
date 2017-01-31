2d7ce6fdfe29ad5ccafb2beb38db0827c2a3dc11
Kino Roy <kroy@sfu.ca>
2017-01-31 13:03:41 -0800

    Initial Commit

---
+++ RenewPass/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RenewPass
+//
+//  Created by Kino Roy on 2017-01-31.
+//  Copyright © 2017 Kino Roy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
