b75f98a05a2e647b79b09c48dd0842ea4dcfd520
Kino Roy <kroy@sfu.ca>
2017-02-01 21:00:35 -0800

    Clears unnecessary libraries/frameworks

--- Carthage/Checkouts/hpple/Package.swift
+++
@@ -1,9 +0,0 @@
-import PackageDescription
-
-let package = Package(
-  name: "hpple",
-  targets: []
-  //dependencies: [
-  //      .Package(url: "https://github.com/mkoehnke/Clibxml2.git", majorVersion: 1)
-  //]
-)
