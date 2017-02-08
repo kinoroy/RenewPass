adaea1999a4b44246b2640c3159f7a6ceb6b17b7
Kino Roy <kroy@sfu.ca>
2017-02-08 11:37:03 -0800

    Adds UBC Authentication JavaScript

---
+++ RenewPass/JavaScript/Authenticate_UBC.js
@@ -0,0 +1,12 @@
+/* This script fills out the authentication form for CWL, UBC's authentication service
+ Requires: - storedUsername: the UBC users's username
+ - storedPassword: the UBC user's password
+ 
+ storedUsername and storedPassword are inserted at runtime
+ (from the core data storage and keychain respectively) right before injecting the script into the webview
+ */
+
+var form = document.querySelector("[name=loginForm]");
+document.querySelector("#j_username").value = "storedUsername";
+document.querySelector("#password").value = "storedPassword";
+form.submit();
