f5d3d656ff111da78dfa34c49486d14e425e5820
Kino Roy <kroy@sfu.ca>
2017-02-19 18:39:23 -0800

    Deletes the stack view for showing school button objects and replaces it with a collection view

--- RenewPass/SignInViewController.swift
+++ RenewPass/SignInViewController.swift
@@ -9,29 +9,34 @@
 import UIKit
 import CoreData
 
-class SignInViewController: UIViewController, UITextFieldDelegate {
+class SignInViewController: UIViewController, UITextFieldDelegate, UICollectionViewDelegate, UICollectionViewDataSource {
     
     // MARK: - Proporties
     
-    @IBOutlet weak var stackView: SchoolSelectorStackView!
-    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     var accounts:[NSManagedObject]!
     @IBOutlet weak var usernameLabel: UILabel!
+    var buttons:[UIButton] = []
+    var selectedButton:UIButton? = nil
     
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        NotificationCenter.default.addObserver(forName: Notification.Name("schoolWasSelected"), object: nil, queue: nil, using: schoolWasSelected)
-        
-        NotificationCenter.default.post(name: Notification.Name("schoolWasSelected"), object: nil)
-        
+        // Setup delegates
         self.usernameField.delegate = self
         self.passwordField.delegate = self
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
         
-        /* If you're running a debug build, the login screen can auto-populate your testing login, 
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        /* If you're running a debug build, the login screen can auto-populate your testing login,
          put your username, password and school code (School code is the raw value of your school enum in Schools.swift), into DebugUserInfo.plist.
          
          !!!! WARNING: BEFORE MODIFYING DEBUGUSERINFO.PLIST YOU MUST RUN "git update-index --assume-unchanged DebugUserInfo.plist" TO PREVENT YOUR LOGIN INFO FROM BEING SENT TO THE REPO !!!! */
@@ -39,21 +44,14 @@ class SignInViewController: UIViewController, UITextFieldDelegate {
             if let userDataPListURL = Bundle.main.url(forResource: "DebugUserInfo", withExtension: "plist"),
                 let userDataFile = try? Data(contentsOf: userDataPListURL) {
                 if let userDataDict = try? PropertyListSerialization.propertyList(from: userDataFile, options: [], format: nil) as? [String: Any] {
-
+                    
                     self.usernameField.text = userDataDict?["Username"] as? String ?? ""
                     self.passwordField.text = userDataDict?["Password"] as? String ?? ""
-                    let matchingSchoolButtons = self.stackView.buttons.filter {$0.tag == userDataDict?["School Code"] as? Int ?? 1}
-                    if matchingSchoolButtons.count > 0 { self.stackView.schoolSelected(sender: matchingSchoolButtons[0]) }
+                    let matchingSchoolButtons = self.buttons.filter {$0.tag == userDataDict?["School Code"] as? Int ?? 1}
+                    if matchingSchoolButtons.count > 0 { self.schoolSelected(sender: matchingSchoolButtons[0]) }
                 }
             }
         #endif
-        
-    }
-
-    override func viewDidLayoutSubviews() {
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        let height = self.stackView.frame.size.height
-        self.scrollView.contentSize = CGSize(width: stackView.width * 1.05, height: height)
 
     }
     
@@ -76,24 +74,59 @@ class SignInViewController: UIViewController, UITextFieldDelegate {
             return
         }
         
-        AccountManager.saveAccount(username: usernameField.text!, password: passwordField.text!, schoolRaw: Int16((stackView.selectedButton?.tag)!))
+        AccountManager.saveAccount(username: usernameField.text!, password: passwordField.text!, schoolRaw: Int16((self.selectedButton?.tag)!))
         
         self.dismiss(animated: true) {
             
         }
         
     }
+    
+    // MARK: - UICollectionViewDataSource
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return Schools.orderedSchools.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "schoolCell", for: indexPath)
+        
+        let schoolObj = School(school: Schools.orderedSchools[indexPath.row])
+        let unSelectedimage = UIImage(named: "\(schoolObj.shortName)_unchecked")
+        let selectedimage = UIImage(named: "\(schoolObj.shortName)_checked")
+        let frame = CGRect(x: 0 , y: 0, width: cell.frame.size.width, height: cell.frame.size.height)
+        
+        let button = UIButton(frame: frame)
+        button.setImage(unSelectedimage, for: .normal)
+        button.setImage(selectedimage, for: .selected)
+        button.tag = Int(schoolObj.school.rawValue)
+        button.addTarget(self, action: #selector(schoolSelected), for: .touchUpInside)
+        
+        buttons.append(button)
+
+        cell.contentView.addSubview(button)
+        
+        return cell
+    }
 
     // MARK: - Private methods
     
-    private func schoolWasSelected(notification:Notification) {
-        if let selectedSchool = stackView.selectedButton?.tag {
-            let school = School(school: Schools(rawValue: Int16(selectedSchool))!)
-            self.usernameLabel.text = school.userNameLabel
-            self.usernameField.placeholder = school.userNamePlaceHolder
-
+    func schoolSelected(sender: UIButton) {
+        // If there is any current school selected, de-select it
+        if selectedButton != nil {
+            selectedButton?.isSelected = false
         }
+        
+        // Now select the new school button
+        DispatchQueue.main.async {
+            sender.isSelected = !sender.isSelected
+        }
+        selectedButton = sender
+        
+        let school = School(school: Schools(rawValue: Int16(sender.tag))!)
+        self.usernameLabel.text = school.userNameLabel
+        self.usernameField.placeholder = school.userNamePlaceHolder
+            
     }
+    
 
     // MARK: - Status Bar
     override var prefersStatusBarHidden: Bool {
