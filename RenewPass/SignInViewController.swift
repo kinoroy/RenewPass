7dbebfa5b5da757ee2121862522766fb6590ad32
Kino Roy <kroy@sfu.ca>
2017-02-27 20:04:39 -0800

    Cleanup and comments for the SignInViewController

--- RenewPass/SignInViewController.swift
+++ RenewPass/SignInViewController.swift
@@ -9,15 +9,20 @@
 import UIKit
 import CoreData
 
+/// The view controller which represents the login screen of the app
 class SignInViewController: UIViewController, UITextFieldDelegate {
     
     // MARK: - Proporties
     
+    /// The username field of the login screen
     @IBOutlet weak var usernameField: UITextField!
+    /// The password field of the login screen
     @IBOutlet weak var passwordField: UITextField!
-    var accounts:[NSManagedObject]!
+    // The label which describes the type of input for the username field
     @IBOutlet weak var usernameLabel: UILabel!
+    // The button which represents the school the user chose in the school selection view
     var schoolSelected:UIButton!
+    // A button which brings up a list of password managers
     @IBOutlet weak var onePassButton: UIButton!
     
     
@@ -26,14 +31,14 @@ class SignInViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Change the nav bar text 
+        //Change the nav bar text from "Select School" to "Sign In"
         self.navigationController?.title = "Sign In"
         
         // Setup delegates
         self.usernameField.delegate = self
         self.passwordField.delegate = self
         
-        //change username label and placeholder to corespond to school login
+        //Change username label and placeholder to corespond to school login
         let school = School(school: Schools(rawValue: Int16(schoolSelected.tag))!)
         self.usernameLabel.text = school.userNameLabel
         self.usernameField.placeholder = school.userNamePlaceHolder
@@ -79,14 +84,18 @@ class SignInViewController: UIViewController, UITextFieldDelegate {
     // MARK: - UITextFields
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Dismiss the keyboard when the user hits return
         textField.endEditing(true)
         return false
     }
     
     // MARK: - Navigation
 
+    /// Called when the user clicks the submit button on the login screen
     @IBAction func clickSubmitButton(_ sender: Any) {
         
+        // Asset the username and password fields are both not empty before continuing
+        // otherwise, display an error message and do not continue
         guard !(usernameField.text?.isEmpty)! && !(passwordField.text?.isEmpty)! else {
             let alert = UIAlertController(title: "Error", message: "Username and password can not be empty", preferredStyle: .alert)
             let okAction = UIAlertAction(title: "OK", style: .default)
@@ -95,8 +104,13 @@ class SignInViewController: UIViewController, UITextFieldDelegate {
             return
         }
         
-        AccountManager.saveAccount(username: usernameField.text!, password: passwordField.text!, schoolRaw: Int16((schoolSelected.tag)))
+        // Save the username, password, and selected school into CoreData
+        // If the account manager reports the either CoreData or Keychain couldn't save the credentials, the app will crash
+        guard AccountManager.saveAccount(username: usernameField.text!, password: passwordField.text!, schoolRaw: Int16((schoolSelected.tag))) else {
+            fatalError("Couldn't save account data")
+        }
         
+        // Dismiss the sign-in screen
         self.dismiss(animated: true) {
             
         }
@@ -105,15 +119,19 @@ class SignInViewController: UIViewController, UITextFieldDelegate {
 
     // MARK: - 1Password
     
+    /// Calls the One-Password extension when the user clicks the one-password button
     @IBAction func callOnePass(_ sender: Any) {
         
+        // Create a school object from the selected school
         let school = School(school: Schools(rawValue: Int16(schoolSelected.tag))!)
         
+        // Call the One-Password extension to show an action sheet with possible password managers
         OnePasswordExtension.shared().findLogin(forURLString: school.urlString, for: self, sender: sender, completion: { (loginDictionary, error) -> Void in
             if loginDictionary == nil {
                 return
             }
             
+            // Get the username and password from the password manager
             self.usernameField.text = loginDictionary?[AppExtensionUsernameKey] as? String
             self.passwordField.text = loginDictionary?[AppExtensionPasswordKey] as? String
         })
@@ -121,6 +139,7 @@ class SignInViewController: UIViewController, UITextFieldDelegate {
     
 
     // MARK: - Status Bar
+    // Hide the status bar on debug builds
     override var prefersStatusBarHidden: Bool {
         #if DEBUG
             return true
