410188448fc354b8dfb018f6bec094212b7d6e54
Kino Roy <kroy@sfu.ca>
2017-02-27 19:46:37 -0800

    Comments and guard statements in AccountManager saveAccount

--- RenewPass/AccountManager.swift
+++ RenewPass/AccountManager.swift
@@ -14,33 +14,53 @@ class AccountManager {
     
     // MARK: - Public Methods
     
-    static public func saveAccount(username: String, password:String, schoolRaw:Int16) {
-        // Save username and school through CoreData
-        let appDelegate =
-            UIApplication.shared.delegate as! AppDelegate
+    /// Saves the username and school into an account object in core data, and the password into the iOS keychain
+    /// - Parameters:
+    ///     - username: A string representing the username for the user's chosen school, (could contain letters, numbers or symbols (@,. for email based school logins)
+    ///     - password: A string representing the password associate with the school account
+    ///     - schoolRaw: A 16-bit integer representing the school code for the user's school (Can be 1-10 inclusive)
+    /// - Returns: A boolean representing whether the account was saved correctly or not
+    static public func saveAccount(username: String, password:String, schoolRaw:Int16) -> Bool {
         
+        // Core Data:
+        
+        // Get the app delegate
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return false
+        }
+        
+        // Get the managed context for CoreData
         let managedContext = appDelegate.persistentContainer.viewContext
         
-        let entity =  NSEntityDescription.entity(forEntityName: "Account",
-                                                 in:managedContext)
+        // Get the account entity from the managed context
+        guard let entity =  NSEntityDescription.entity(forEntityName: "Account", in:managedContext) else {
+            return false
+        }
         
-        let account = NSManagedObject(entity: entity!,
+        // Get the account object from the core data entity
+        let account = NSManagedObject(entity: entity,
                                       insertInto: managedContext)
         
-        
+        // Insert the given credentials into the account object
         account.setValue(username, forKey: "username")
         account.setValue(schoolRaw, forKey: "schoolRaw")
         
-        
+        // Save into CoreData
         appDelegate.saveContext()
         
-        // Save password through keychain 
+        // Keychain:
+        
+        // Get the shared Keychain helper object
         let keychain = KeychainSwift()
         
-        if !keychain.set(password, forKey: "accountPassword", withAccess: KeychainSwiftAccessOptions.accessibleAfterFirstUnlock) {
-            fatalError("Couldn't store in the keychain")
+        // Save the user's password in the keychain with accessibility after first unlock
+        guard keychain.set(password, forKey: "accountPassword", withAccess: KeychainSwiftAccessOptions.accessibleAfterFirstUnlock) else {
+            return false
         }
         
+        return true
     }
+    
+    
     
 }
