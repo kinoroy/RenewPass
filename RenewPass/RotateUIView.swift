dbb3d1b8aaffef9410f646b7d11821998333c4b7
Kino <kroy@sfu.ca>
2019-09-28 23:46:21 -0700

    Fix deprecation warnings

--- RenewPass/RotateUIView.swift
+++ RenewPass/RotateUIView.swift
@@ -12,7 +12,7 @@ extension UIView {
     func rotate360Degrees(duration: CFTimeInterval = 1.0, completionDelegate: AnyObject? = nil) {
         let rotateAnimation = CABasicAnimation(keyPath: "transform.rotation")
         rotateAnimation.fromValue = 0.0
-        rotateAnimation.toValue = CGFloat(M_PI * 2.0)
+        rotateAnimation.toValue = CGFloat.pi * 2.0
         rotateAnimation.duration = duration
         
         if let delegate: CAAnimationDelegate = completionDelegate as? CAAnimationDelegate {
